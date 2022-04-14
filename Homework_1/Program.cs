c64e309bc5a8b45be98953df6ef746f613a15bca
Alex Green <a_green@inbox.ru>
2022-04-14 19:20:47 +0300

    Fourth task done

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -47,7 +47,6 @@ Console.WriteLine("Больше число " + max);
 4 -> да
 -3 -> нет
 7 -> нет
-*/
 
 Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
@@ -60,6 +59,7 @@ else
 {
     Console.WriteLine("Это число нечётное.");
 }
+*/
 
 /*
 Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
@@ -67,3 +67,13 @@ else
 5 -> 2, 4
 8 -> 2, 4, 6, 8
 */
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int current = 2;
+
+while(current < num)
+{
+    Console.Write(current + " ");
+    current = current + 2;
+}
