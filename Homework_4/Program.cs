e077387eaa77ebc5acc6540c60648b40376ffa2a
Alex Green <a_green@inbox.ru>
2022-04-24 09:24:22 +0300

    second task done

--- Homework_4/Program.cs
+++ Homework_4/Program.cs
@@ -1,7 +1,7 @@
 ﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
 Напишите программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2
-*/
+
 
 int[] CreateRandomArray(int size, int min, int max)
 {
@@ -38,12 +38,49 @@ void CountEven(int[] array)
 int[] myArray = CreateRandomArray(4, 100, 999);
 ShowArray(myArray);
 CountEven(myArray);
+*/
 
 /*Задача 36: Задайте одномерный массив, заполненный случайными числами.
 Найдите сумму элементов, стоящих на нечётных позициях.
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0
 */
+
+int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+void EvenPositionSum(int[] array)
+{
+    int evenPosition = 0;
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 > 0)
+            evenPosition += array[i];
+    }
+    Console.WriteLine("Сумма элементов: " + evenPosition);
+}
+
+int[] myArray = CreateRandomArray(4, -100, 100);
+ShowArray(myArray);
+EvenPositionSum(myArray);
 
 /*Задача 38: Задайте массив вещественных чисел.
 Найдите разницу между максимальным и минимальным элементов массива.
