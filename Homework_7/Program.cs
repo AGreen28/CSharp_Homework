82ee0ef5bff0ba8f4cf2a00062b59898c0754beb
Alex Green <a_green@inbox.ru>
2022-05-15 10:41:35 +0300

    second task done

--- Homework_7/Program.cs
+++ Homework_7/Program.cs
@@ -4,7 +4,7 @@ m = 3, n = 4.
 0,5 7 -2 -0,2
 1 -3,3 8 -9,9
 8 7,8 -7,1 9
-*/
+
 
 Console.Write("Введите количество строк: ");
 int m = Convert.ToInt32(Console.ReadLine());
@@ -20,6 +20,7 @@ for(int i = 0; i < m; i++)
     }
     Console.WriteLine();
 }
+*/
 
 /*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
 и возвращает значение этого элемента или же указание, что такого элемента нет.
@@ -29,6 +30,28 @@ for(int i = 0; i < m; i++)
 8 4 2 4
 17 -> такого числа в массиве нет
 */
+
+int[,] matrix = new int[3,4];
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(0,10);
+        Console.Write(matrix[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите номер строки: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(m <= matrix.GetLength(0) && n <= matrix.GetLength(1))
+    Console.WriteLine(matrix[m-1,n-1]);
+else
+    Console.WriteLine("Такого числа в массиве нет.");
+
 
 /*Задача 52. Задайте двумерный массив из целых чисел.
 Найдите среднее арифметическое элементов в каждом столбце.
