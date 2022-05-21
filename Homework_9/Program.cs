fb6a88c9ddc0a2cf5184aaee157e5e5dc458087d
Alex Green <a_green@inbox.ru>
2022-05-21 15:31:22 +0300

    second task done

--- Homework_9/Program.cs
+++ Homework_9/Program.cs
@@ -2,7 +2,7 @@
 которая найдёт сумму натуральных элементов в промежутке от M до N.
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
-*/
+
 
 Console.Write("Введите первое значение: ");
 int n = Convert.ToInt32(Console.ReadLine());
@@ -25,9 +25,20 @@ else
     m++;
     }
 Console.WriteLine(sum);
-
+*/
 
 /*Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
 453 -> 12
 45 -> 9
 */
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+while(num > 0)
+{
+    sum = sum + num % 10;
+    num = num / 10;
+}
+Console.WriteLine(sum);
